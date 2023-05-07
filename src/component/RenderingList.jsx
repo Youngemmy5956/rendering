c6c6b76d47ee158ef651181fa128ce89a43a57a0
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-05-07 08:09:44 +0100

    new

--- src/component/RenderingList.jsx
+++ src/component/RenderingList.jsx
@@ -14,24 +14,6 @@ const recipes = [{
     ingredients: ["'chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'"]
   }];
 
-//   const RenderingList = ({ recipes }) => {
-//     return (
-//       <div>
-//         <h2>Recipes</h2>
-//         {recipes.map(recipe => (
-//           <div key={recipe.id}>
-//             <h2>{recipe.name}</h2>
-//             <ul>
-//               {recipe.ingredients.map(ingredient => (
-//                 <li key={ingredient}>{ingredient}</li>
-//               ))}
-//             </ul>
-//           </div>
-//         ))}
-//       </div>
-//     );
-//   }
-
 
   const RenderingList = () => {
     return (
